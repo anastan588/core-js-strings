f02bd8cfa436214b417ab0849ca803b2711683f7
Anastasiya Andronova <anastasiya5.88@mail.ru>
2023-11-21 00:10:12 +0300

    feat: add solutions of string task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -316,8 +316,8 @@ function formatTime(minutes, seconds) {
  *   reverseString('abcdef') => 'fedcba'
  *   reverseString('12345') => '54321'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
 
 /**
@@ -331,8 +331,8 @@ function reverseString(/* str */) {
  *   orderAlphabetically('textbook') => 'bekoottx'
  *   orderAlphabetically('abc123xyz') => '123abcxyz'
  */
-function orderAlphabetically(/* str */) {
-  throw new Error('Not implemented');
+function orderAlphabetically(str) {
+  return str.split('').sort().join('');
 }
 
 /**
@@ -347,8 +347,8 @@ function orderAlphabetically(/* str */) {
  *   containsSubstring('JavaScript is Fun', 'Python') => false
  *   containsSubstring('12345', '34') => true
  */
-function containsSubstring(/* str, substring */) {
-  throw new Error('Not implemented');
+function containsSubstring(str, substring) {
+  return str.includes(substring);
 }
 
 /**
@@ -382,8 +382,14 @@ function countVowels(/* str */) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const reverseStr = str.replace(/[\W_]/g, '').split('').reverse().join('');
+  // console.log(str);
+  // console.log(reverseStr);
+  if (reverseStr.toLowerCase() === str.replace(/[\W_]/g, '').toLowerCase()) {
+    return true;
+  }
+  return false;
 }
 
 /**
